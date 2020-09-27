1be9c916afea858de7e5516331f8b2b7fa2a5440
Tran Dang Tin <thantienbut@gmail.com>
2020-09-27 22:48:08 +0700

    add shortcode rate_buy and rate_sale

--- client.php
+++ client.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Exchange rate management client
  * Description: Exchange rate management client
@@ -7,34 +8,36 @@
  */
 
 
- // Add javascript to client
-add_action( 'wp_footer', 'load_client_style' );
-function load_client_style() {
-    wp_register_style( 'exchange_rate-jquery-ui', plugins_url( '/css/lib/jquery-ui.css', __FILE__ ) );
-    wp_enqueue_style( 'exchange_rate-jquery-ui' );
+// Add javascript to client
+add_action('wp_footer', 'load_client_style');
+function load_client_style()
+{
+    wp_register_style('exchange_rate-jquery-ui', plugins_url('/css/lib/jquery-ui.css', __FILE__));
+    wp_enqueue_style('exchange_rate-jquery-ui');
 
-    wp_register_style( 'exchange_rate', plugins_url( '/css/client.css', __FILE__ ) );
-   wp_enqueue_style( 'exchange_rate' );
-    
+    wp_register_style('exchange_rate', plugins_url('/css/client.css', __FILE__));
+    wp_enqueue_style('exchange_rate');
+
     // wp_enqueue_script( 'exchange_rate-jquery', plugins_url( '/js/lib/jquery.min.js', __FILE__ ) );
-    wp_enqueue_script( 'exchange_rate-jquery-ui', plugins_url( '/js/lib/jquery-ui.min.js', __FILE__ ) );
-    
-    wp_enqueue_script( 'exchange_rate-moment', plugins_url( '/js/lib/moment.min.js', __FILE__ ) );
-    wp_enqueue_script( 'exchange_rate-moment', plugins_url( '/js/lib/moment-timezone-with-data.js', __FILE__ ) );
-    wp_enqueue_script( 'exchange_rate-chart-js', plugins_url( '/js/lib/Chart.min.js', __FILE__ ) );
-    wp_enqueue_script( 'exchange_rate-utils', plugins_url( '/js/lib/utils.js', __FILE__ ) );
+    wp_enqueue_script('exchange_rate-jquery-ui', plugins_url('/js/lib/jquery-ui.min.js', __FILE__));
 
-    wp_enqueue_script( 'exchange_rate_common', plugins_url( '/js/common.js', __FILE__ ) );
-    wp_enqueue_script( 'exchange_rate', plugins_url( '/js/client.js', __FILE__ ) );
- 
-    wp_localize_script( 'exchange_rate', 'exchange_rate_js_vars', array( 'ajax_image' => plugin_dir_url( __FILE__ ) . 'images/loading.gif', 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
- }
+    wp_enqueue_script('exchange_rate-moment', plugins_url('/js/lib/moment.min.js', __FILE__));
+    wp_enqueue_script('exchange_rate-moment', plugins_url('/js/lib/moment-timezone-with-data.js', __FILE__));
+    wp_enqueue_script('exchange_rate-chart-js', plugins_url('/js/lib/Chart.min.js', __FILE__));
+    wp_enqueue_script('exchange_rate-utils', plugins_url('/js/lib/utils.js', __FILE__));
 
- // ==========================Area rate buy and rate sale start ========================
- /**
+    wp_enqueue_script('exchange_rate_common', plugins_url('/js/common.js', __FILE__));
+    wp_enqueue_script('exchange_rate', plugins_url('/js/client.js', __FILE__));
+
+    wp_localize_script('exchange_rate', 'exchange_rate_js_vars', array('ajax_image' => plugin_dir_url(__FILE__) . 'images/loading.gif', 'ajaxurl' => admin_url('admin-ajax.php')));
+}
+
+// ==========================Area rate buy and rate sale start ========================
+/**
  * Money change service
  */
-function money_rate_buy_sale_shortcode() {
+function money_rate_buy_sale_shortcode()
+{
     $rates = get_rate_table();
     $rateBuy = '';
     $rateSale = '';
@@ -55,12 +58,12 @@ function money_rate_buy_sale_shortcode() {
     $Content .= '           <td class="title">BÁN RA</td>';
     $Content .= '       <tr>';
     $Content .= '       <tr>';
-    $Content .= '           <td><div><span class="rate_buy">'. $rateBuy . '</span><span class="arow-up"></span></div></td>';
-    $Content .= '           <td><div><span class="rate_sale">'. $rateSale . '</span><span class="arow-down"></span></div></td>';
+    $Content .= '           <td><div><span class="rate_buy">' . $rateBuy . '</span><span class="arow-up"></span></div></td>';
+    $Content .= '           <td><div><span class="rate_sale">' . $rateSale . '</span><span class="arow-down"></span></div></td>';
     $Content .= '       <tr>';
     $Content .= '   </table>';
     $Content .= '</div>';
-	 
+
     return $Content;
 }
 
@@ -69,19 +72,21 @@ add_shortcode('money-rate-buy-sale', 'money_rate_buy_sale_shortcode');
 /**
  * Get data table wp_rate
  */
-function get_rate_table() {
+function get_rate_table()
+{
     global $wpdb;
     $rate = $wpdb->get_results("SELECT * FROM wp_rate");
 
     return $rate;
 }
 
- // ==========================Area rate buy and rate sale end===========================
+// ==========================Area rate buy and rate sale end===========================
 
 /**
  * Money change service
  */
-function money_change_shortcode($atts) {
+function money_change_shortcode($atts)
+{
     $VN_CN = 'VN_CN';
     $CN_VN = 'CN_VN';
     $TTH = 'TTH';
@@ -112,44 +117,44 @@ function money_change_shortcode($atts) {
     }
 
     $Content = '<div class="money-change">';
-    $Content .= '   <input class="rate-buy" type="hidden" value="'. $rateBuy . '" />';
-    $Content .= '   <input class="rate-sale" type="hidden" value="'. $rateSale . '" />';    
+    $Content .= '   <input class="rate-buy" type="hidden" value="' . $rateBuy . '" />';
+    $Content .= '   <input class="rate-sale" type="hidden" value="' . $rateSale . '" />';
     if ($type == $CN_VN) {
         $Content .= '   <div class="title">Đổi tiền Trung sang tiền Việt</div>';
-        $Content .= '   <input class="fee_withdraw_alipay_wechat" type="hidden" value="'. $fee_withdraw_alipay_wechat . '" />';
-        $Content .= '   <input class="difference_rate_tm_and_tk" type="hidden" value="'. $difference_rate_tm_and_tk . '" />';
+        $Content .= '   <input class="fee_withdraw_alipay_wechat" type="hidden" value="' . $fee_withdraw_alipay_wechat . '" />';
+        $Content .= '   <input class="difference_rate_tm_and_tk" type="hidden" value="' . $difference_rate_tm_and_tk . '" />';
     } else if ($type == $VN_CN) {
         $Content .= '   <div class="title">Đổi tiền Việt sang tiền Trung</div>';
     } else if ($type == $TTH) {
         $Content .= '   <div class="title">Thanh toán hộ</div>';
     }
-    
+
     $Content .= '   <table>';
     $Content .= '       <tr>';
     $Content .= '           <td>';
 
     if ($type == $CN_VN) {
-        $Content .= '               <select class="'. $CN_VN. '">';
+        $Content .= '               <select class="' . $CN_VN . '">';
         $Content .= '                   <option value="0">Chọn nguồn tiền trung</option>';
         $Content .= '                   <option value="1">Wechat, alipay</option>';
         $Content .= '                   <option value="2">Tiền mặt</option>';
         $Content .= '                   <option value="3">Tài khoản</option>';
         $Content .= '               </select>';
     } else if ($type == $VN_CN) {
-        $Content .= '               <select class="'. $VN_CN. '">';
+        $Content .= '               <select class="' . $VN_CN . '">';
         $Content .= '                   <option value="0">Chọn nguồn tiền trung</option>';
         $Content .= '                   <option value="1">Nạp tệ wechat, alipay</option>';
         $Content .= '                   <option value="2">Chuyển khoản</option>';
         $Content .= '               </select>';
     } else if ($type == $TTH) {
-        $Content .= '               <select class="'. $TTH .'">';
+        $Content .= '               <select class="' . $TTH . '">';
         $Content .= '                   <option value="0">Chọn trang cần thanh toán</option>';
         $Content .= '                   <option value="1">Taobao</option>';
         $Content .= '                   <option value="2">1688</option>';
         $Content .= '                   <option value="3">Khác</option>';
         $Content .= '               </select>';
     }
-    
+
     $Content .= '           </td>';
     $Content .= '           <td>';
     $Content .= '               <div class="money input-money">';
@@ -166,10 +171,10 @@ function money_change_shortcode($atts) {
     $Content .= '       <tr>';
     $Content .= '   </table>';
     $Content .= '   <div class="footer">';
-    $Content .= '       <input class="money-change-type" type="hidden" value="'. $type . '" />';
+    $Content .= '       <input class="money-change-type" type="hidden" value="' . $type . '" />';
     $Content .= '   </div>';
     $Content .= '</div>';
-	 
+
     return $Content;
 }
 
@@ -178,7 +183,8 @@ add_shortcode('money-change', 'money_change_shortcode');
 /**
  * Get data table wp_service_change_money_option
  */
-function get_wp_service_change_money_option() {
+function get_wp_service_change_money_option()
+{
     global $wpdb;
     $wp_service_change_money_option = $wpdb->get_results("SELECT * FROM wp_service_change_money_option");
 
@@ -187,10 +193,11 @@ function get_wp_service_change_money_option() {
 
 
 // ================================Area of Chart start================================
- /**
+/**
  * Chart history shortcode
  */
-function chart_history_shortcode() {
+function chart_history_shortcode()
+{
 
     $Content = '<div class="chart_menu hide">';
     $Content .= '   <div class="form-search">';
@@ -214,10 +221,56 @@ function chart_history_shortcode() {
     $Content .= '       </div>';
     $Content .= '   </div>';
     $Content .= '</div>';
-	 
+
     return $Content;
 }
 
 add_shortcode('chart-history', 'chart_history_shortcode');
 
 // ================================Area of Chart end================================
+
+/**
+ * Rate buy shortcode
+ */
+function rate_buy_shortcode($atts)
+{
+
+    // Get data rate
+    $rates = get_rate_table();
+    $rateBuy = 0;
+    if (count($rates) > 0) {
+        $rate = $rates[0]->rate;
+        $rateX = $rates[0]->rate_buy;
+
+        $rateBuy = $rate - $rateX;
+    }
+
+    $Content = $rateBuy;
+
+    return $Content;
+}
+
+add_shortcode('get_rate_buy', 'rate_buy_shortcode');
+
+/**
+ * Rate sale shortcode
+ */
+function rate_sale_shortcode($atts)
+{
+
+    // Get data rate
+    $rates = get_rate_table();
+    $rateSale = '';
+    if (count($rates) > 0) {
+        $rate = $rates[0]->rate;
+        $rateY = $rates[0]->rate_sale;
+
+        $rateSale = $rate + $rateY;
+    }
+
+    $Content = $rateSale;
+
+    return $Content;
+}
+
+add_shortcode('get_sale_buy', 'rate_sale_shortcode');
