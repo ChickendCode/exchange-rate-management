cc5101521d82b3d69be199f47c0a8163ee676f19
Tran Dang Tin <thantienbut@gmail.com>
2020-10-02 23:11:53 +0700

    add new

---
+++ schedule.php
@@ -0,0 +1,26 @@
+<?php
+
+// ================================ scheduled start =============================================
+global $wpdb; // this is how you get access to the database
+    
+// Get data rate
+$rates = $wpdb->get_results("SELECT * FROM wp_rate");
+$rateBuy = 0;
+$rateSale = 0;
+if (count($rates) > 0) {
+    $rate = $rates[0]->rate;
+    $rateX = $rates[0]->rate_buy;
+    $rateY = $rates[0]->rate_sale;
+
+    $rateBuy = $rate - $rateX;
+    $rateSale = $rate + $rateY;
+}
+
+$datepicker = date('Y-m-d');
+
+$query = "INSERT INTO `wp_rate_history`(`rate_buy`, `rate_sale`, `date`) 
+            VALUES (". $rateBuy .", ". $rateSale .", '". $datepicker ."')";
+
+$wpdb->query( $query );
+
+// ================================ scheduled end =============================================
